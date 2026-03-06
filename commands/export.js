4137f998872722639ffe66212f93f54b4bb18b82
lizereride90 <rawlex126@gmail.com>
2026-03-06 21:15:19 +0530

    Create commands/export.js file

---
+++ commands/export.js
@@ -0,0 +1,9 @@
+// Commands export file
+
+// This file exports all commands for the Discord server maker
+
+module.exports = {
+    // Example command
+    exampleCommand: require('./exampleCommand')
+    // Add your commands here
+};
